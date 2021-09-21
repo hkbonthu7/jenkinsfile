621b052e57feff71db61dcf8937f248f57248348
hkbonthu7 <86041753+hkbonthu7@users.noreply.github.com>
2021-09-21 11:02:18 +0530

    Update jenkinsscript.sh

--- jenkinsscript.sh
+++ jenkinsscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# this script is only tested on ubuntu xenial
+# this script is only tested on ubuntu xenial....
 
 # install docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
